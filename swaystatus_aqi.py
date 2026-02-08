491f09c9e43db542f115bdc3ebcc906305c8de72
Anya-Desdein <heavelyn1999@gmail.com>
2026-02-08 16:14:03 +0100

    Take location from env variable and display data
    Display temperature, air quality, humidity, wind etc.

--- swaystatus_aqi.py
+++ swaystatus_aqi.py
@@ -5,20 +5,72 @@ import os
 import urllib.request
 import time
 
+
 # Configuration
-TOKEN = os.environ.get("TOKEN", "")
-DATA_FILE = os.environ.get("DATA_FILE", "/tmp/swaystatus/swaystatus_airquality")
-URL = f"https://api.waqi.info/feed/wroclaw/?token={TOKEN}"
+SWAYSTATUS_TOKEN = os.environ.get("SWAYSTATUS_TOKEN", "")
+
+SWAYSTATUS_DATA_FILE = os.environ.get("SWAYSTATUS_DATA_FILE", "/tmp/swaystatus/swaystatus_airquality")
+SWAYSTATUS_FORMATTED_FILE = SWAYSTATUS_DATA_FILE + "_formatted"
+
+SWAYSTATUS_LOCATION = os.environ.get("SWAYSTATUS_LOCATION", "")
+
+URL = f"https://api.waqi.info/feed/{SWAYSTATUS_LOCATION}/?token={SWAYSTATUS_TOKEN}"
+
+
+def get_color(t, v):
+    # Pollutants (AQI scale)
+    if t in ["pm25", "no2", "co", "so2", "pm10", "o3", "aqi"]:
+        return "⚪" if v<=0 else "🔵" if v<=25 else "🟢" if v<=50 else "🟡" if v<=100 else "🟠" if v<=150 else "🔴" if v<=200 else "🟣" if v<=300 else "🟤"
+    
+    return "⚪"
+
+
+def format_aqi(full_data):
+    try:
+        d = full_data.get("data", {}).get("iaqi", {})
+        
+        # Extract name
+        full_name = full_data.get("data", {}).get("city", {}).get("name", "N/A")
+        point_name = full_name.split("(")[0].strip()
+        
+        pv = d.get("pm25",{}).get("v",-1)
+        nv = d.get("no2",{}).get("v",-1)
+        cv = d.get("co",{}).get("v",-1)
+        hv = d.get("h",{}).get("v",-1)
+        pres = d.get("p",{}).get("v",-1)
+        tv = d.get("t",{}).get("v",-273.15) # Absolute zero
+        wv = d.get("w",{}).get("v",36000) # Speed of sound
+
+        pc  = get_color("pm25", pv)
+        nc  = get_color("no2", nv)
+        coc = get_color("co", cv)
+        h, pres, t, w = hv, pres, tv, wv
+        name = point_name
+        
+        return f"{name}: PM2.5{pc}{pv} | NO2{nc}{nv} | CO{coc}{cv} | 🌡️{t}°C | 💧{h}% | 🗜{pres}hPa | 💨{w}m/s"
+    except Exception:
+        return "N/A: PM2.5⚪-1 | NO2⚪-1 | CO⚪-1 | 🌡️-273.15°C | 💧-1% | 🗜-1hPa | 💨36000m/s"
 
 def fetch_and_save():
     try:
         with urllib.request.urlopen(URL) as response:
             data = json.load(response)
         
-        tmp_file = DATA_FILE + ".tmp"
+        tmp_file = SWAYSTATUS_DATA_FILE + ".tmp"
         with open(tmp_file, "w") as f:
             json.dump(data, f)
-        os.rename(tmp_file, DATA_FILE)
+        os.rename(tmp_file, SWAYSTATUS_DATA_FILE)
+        
+        formatted = format_aqi(data)
+        tmp_fmt = SWAYSTATUS_FORMATTED_FILE + ".tmp"
+        with open(tmp_fmt, "w") as f:
+            f.write(formatted)
+        os.rename(tmp_fmt, SWAYSTATUS_FORMATTED_FILE)
+
+        with open(SWAYSTATUS_DATA_FILE, "r") as f:
+            saved_data = json.load(f)
+        format_aqi(saved_data)
+            
     except Exception as e:
         pass
 
